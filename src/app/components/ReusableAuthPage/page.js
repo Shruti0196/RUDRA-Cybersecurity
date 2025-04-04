ea8ab43799bdad68a3ccd0b0df8831c8cd1f9435
Shruti Prasad <shrutiap2003@gmail.com>
2025-04-04 09:34:40 +0530

    Fixed css

--- src/app/components/ReusableAuthPage/page.js
+++ src/app/components/ReusableAuthPage/page.js
@@ -4,19 +4,29 @@ import CardContent from '@mui/material/CardContent';
 import TextField from '@mui/material/TextField';
 import { CardActions, Button } from '@mui/material';
 import Link from 'next/link';
+import styles from './ReusableAuthPage.module.css'
+import Image from 'next/image';
 
-export default function ReusableAuthPage({ fields, buttonText, authType }) {
+function ReusableAuthPage({ fields, buttonText, authType }) {
     return (
-        <Card>
+        <Card className={styles.card}>
             <CardContent>
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={200}
+                    height={100}
+                />
                 {fields.map((textField) => (
-                    <TextField id="outlined-basic" label={textField} variant="outlined" key={textField} />
+                    <TextField className={styles.textField} id="outlined-basic" label={textField} variant="outlined" key={textField} />
                 ))}
-                {authType === "login" && <Link href="/forgot-password" key="forgot-password">Forgot Password</Link>}
+                {authType === "login" && <Link href="/forgot-password" key="forgot-password"><div className={styles.forgotPassword}>Forgot Password ?</div></Link>}
             </CardContent>
             <CardActions>
-                <Button size="small">{buttonText}</Button>
+                <Button size="small" className={styles.button}>{buttonText}</Button>
             </CardActions>
         </Card>
     )
 }
+
+export default ReusableAuthPage;
